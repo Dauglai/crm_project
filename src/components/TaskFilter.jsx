82a34c14c05456d10b50fdb237aa0dcb76a4a5ca
Dauglai <danil.samartsev.04@gmail.com>
2024-11-01 22:11:37 +0500

    filtration and pagination

--- src/components/TaskFilter.jsx
+++ src/components/TaskFilter.jsx
@@ -2,22 +2,38 @@ import React from "react";
 import MyInput from "./UI/input/MyInput";
 import MySelect from "./UI/select/MySelect";
 
-
-const TaskFilter = ({filter, setFilter}) => {
+const TaskFilter = ({ filter, setFilter }) => {
     return (
         <div>
             <MyInput
                 value={filter.query}
-                onChange={e => setFilter({...filter, query: e.target.value})}
-                placeholder='Поиск'
+                onChange={(e) => setFilter({ ...filter, query: e.target.value })}
+                placeholder="Поиск"
             />
             <MySelect
                 value={filter.sort}
-                onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
-                options={[{name: 'По теме', value: 'name'}, {name: 'По сроку выполнения', value: 'deadline'}]} defaultValue={'Фильтры'}
+                onChange={(selectedSort) => setFilter({ ...filter, sort: selectedSort })}
+                options={[
+                    { name: "По теме", value: "name" },
+                    { name: "По сроку выполнения", value: "deadline" },
+                    { name: "По статусу", value: "status" },
+                ]}
+                defaultValue="Фильтры"
+            />
+            <MySelect
+                value={filter.role}
+                onChange={(selectedRole) => setFilter({ ...filter, role: selectedRole })}
+                options={[
+                    { name: "Автор", value: "author" },
+                    { name: "Адресат", value: "addressee" },
+                    { name: "Соглосователь", value: "coordinator" },
+                    { name: "Наблюдатель", value: "observer" },
+                    { name: "Все с моим участием", value: "all" },
+                ]}
+                defaultValue="Роль"
             />
         </div>
-    )
+    );
 };
 
 export default TaskFilter;
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,94 +1,82 @@
-import React, {useEffect, useRef, useState} from 'react';
-import PostForm from '../components/PostForm';
-import MyModal from '../components/UI/MyModal/MyModal';
-import Loader from '../components/UI/Loader/Loader';
-import MyButton from '../components/UI/button/MyButton';
-import { useTasks } from '../hooks/useTasks';
-import TaskService from '../components/API/PostService';
-import useFetching from '../hooks/useFetching';
-import { getPageCount, getPagesArray } from '../utils/pages';
-import Pagination from '../components/UI/pagination/Pagination';
+import React, { useEffect, useRef, useState } from 'react';
 import TaskTable from "../components/TaskTable";
-import TaskFilter from "../components/TaskFilter";
-import axios from "axios";
+import Pagination from '../components/UI/pagination/Pagination';
 import MySelect from "../components/UI/select/MySelect";
-import {useObserver} from "../hooks/useObserver";
+import axios from "axios";
+import { getPageCount, getPagesArray } from '../utils/pages';
+import Loader from '../components/UI/Loader/Loader';
+import TaskFilter from "../components/TaskFilter";
+import {useTasks} from "../hooks/useTasks";
 
 function Tasks() {
     const [tasks, setTasks] = useState([]);
-    const [error, setError] = useState(null);
-    const [filter, setFilter] = useState({ sort: '', query: '' });
-    const [modal, setModal] = useState(false);
-    const sortedAndSearchedTasks = useTasks(tasks, filter.sort, filter.query);
     const [totalPages, setTotalPages] = useState(0);
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
-    const lastElement = useRef();
-    const observer = useRef();
-    let pagesArray = getPagesArray(totalPages);
+    const [isLoading, setIsLoading] = useState(false);
+    const [filter, setFilter] = useState({sort: '', query: '', role: 'author'});
+    const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = useTasks(tasks, filter.sort, filter.query)
 
-    const [fetchTasks, isTaskLoading, taskError] = useFetching(async (limit, page) => {
+    const fetchTasks = async () => {
+        setIsLoading(true);
         try {
             const response = await axios.get('http://localhost:8000/task/', {
-                params: { _limit: limit, _page: page },
+                params: {
+                    page_size: limit,
+                    page: page,
+                    query: filter.query,
+                    ordering: filter.sort,
+                    role: filter.role,},
                 withCredentials: true,
             });
-            setTasks(response.data);
-            const totalCount = 100
-            console.log('totalCount', totalCount);
-            setTotalPages(getPageCount(totalCount, limit)); // Если -1, показываем все
-        } catch (err) {
-            setError('Failed to load tasks');
-        }
-    });
+            setTasks(response.data.results); // предполагается, что `results` содержит задачи на странице
 
+            // Установка количества страниц на основе общего числа задач
+            const totalCount = response.data.count; // `count` возвращает общее количество задач
+            setTotalPages(getPageCount(totalCount, limit));
+        } catch (err) {
+            console.error('Failed to load tasks:', err);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     useEffect(() => {
-        fetchTasks(limit, page);
-    }, [page, limit]);
-
-    const createTask = (newTask) => {
-        setTasks([...tasks, newTask]);
-        setModal(false);
-    };
+        fetchTasks();
+    }, [page, limit, filter]);
 
     const changePage = (page) => {
         setPage(page);
     };
 
-    const removeTask = (task) => {
-        setTasks(tasks.filter(p => p.id !== task.id));
-    };
-
     return (
         <div className="App">
-            <MyButton onClick={() => setModal(true)}>Open Form</MyButton>
-            <MyModal visible={modal} setVisible={setModal}><PostForm create={createTask}/></MyModal>
             <TaskFilter filter={filter} setFilter={setFilter}/>
             <MySelect
                 value={limit}
                 onChange={(val) => {
                     setLimit(val);
-                    setPage(1); // сбрасываем на первую страницу при изменении лимита
+                    setPage(1); // Переключаемся на первую страницу при изменении лимита
                 }}
-                defaultValue={"Кол-во постов на странице"}
+                defaultValue="Кол-во постов на странице"
                 options={[
-                    {value: 5, name: '5'},
-                    {value: 10, name: '10'},
-                    {value: 25, name: '25'},
-                    {value: -1, name: 'Показать все'},
+                    { value: 5, name: '5' },
+                    { value: 10, name: '10' },
+                    { value: 25, name: '25' },
+                    { value: 100, name: '100' },
                 ]}
             />
-            <hr style={{margin: '15px 0'}}/>
-            {taskError && <p>Fail: {taskError}</p>}
-            <TaskTable tasks={sortedAndSearchedTasks} error={error}/>
-            {isTaskLoading &&
-                <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}><Loader/></div>}
+            <hr style={{ margin: '15px 0' }} />
+            {isLoading ? (
+                <Loader />
+            ) : (<TaskTable tasks={sortedAndSearchedPosts} />
+            )}
             <Pagination
-                pagesArray={pagesArray}
+                pagesArray={getPagesArray(totalPages)}
                 page={page}
-                changePage={changePage}/>
-
+                changePage={changePage}
+            />
         </div>
     );
 }
